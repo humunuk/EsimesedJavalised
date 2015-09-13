478ba2a5ee6ff6e904125049c0a13224f1b4ea56
Siim Kallari <siimkallari@gmail.com>
2015-09-13 16:40:56 +0300

    Java API lihtne example

---
+++ src/EsimeneHalloMaailm/OpinAPI.java
@@ -0,0 +1,18 @@
+package EsimeneHalloMaailm;
+
+/**
+ * Created by Siim Kallari on 13/9/2015.
+ */
+
+import java.util.Arrays;
+
+public class OpinAPI {
+
+    public static void main(String[] args) {
+        int algne = 2;
+        int aste = 3;
+
+        double tulemus = Math.pow(2, 3);
+        System.out.println(tulemus);
+    }
+}
