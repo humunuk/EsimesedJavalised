b66f77dc1c6d38e5b621a75d7e9fa6c08c89e6e1
Siim Kallari <siimkallari@gmail.com>
2015-09-13 15:44:15 +0300

    Sõne konditsion

--- src/EsimeneHalloMaailm/KoditsioonString.java
+++ src/EsimeneHalloMaailm/KoditsioonString.java
@@ -5,6 +5,14 @@ package EsimeneHalloMaailm;
  */
 public class KoditsioonString {
     public static void main(String[] args) {
+        String sihtpunkt = "Tallinn";
 
+        if (sihtpunkt.equals("Tartu")) {
+            System.out.println("Keera paremale");
+        } else if (sihtpunkt.equals("Viljandi")) {
+            System.out.println("Keera vasakule");
+        } else {
+            System.out.println("Sõida otse");
+        }
     }
 }
