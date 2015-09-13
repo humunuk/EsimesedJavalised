da36d21686ead56f80adf1556c2897fc7c21e2ef
Siim Kallari <siimkallari@gmail.com>
2015-09-13 15:32:01 +0300

    Konditsiooni koostamine int-ile

--- src/EsimeneHalloMaailm/Konditsioon.java
+++ src/EsimeneHalloMaailm/Konditsioon.java
@@ -5,6 +5,16 @@ package EsimeneHalloMaailm;
  */
 public class Konditsioon {
     public static void main(String[] args) {
+        int kiirus1 = 50;
+        int kiirus2 = 90;
+        int kiirus3 = 110;
 
+        if (kiirus2 > 90) {
+            System.out.println("Trahv");
+        } else if (kiirus2 == 90) {
+            System.out.println("Napikas");
+        } else {
+            System.out.println("Legaalne");
+        }
     }
 }
