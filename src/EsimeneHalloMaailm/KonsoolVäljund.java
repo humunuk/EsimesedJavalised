287f8b5903b4d91de74f989daa75c3e55a90c8c3
Siim Kallari <siimkallari@gmail.com>
2015-09-13 17:39:41 +0300

    Lihtne väljund

---
+++ src/EsimeneHalloMaailm/KonsoolVäljund.java
@@ -0,0 +1,15 @@
+package EsimeneHalloMaailm;
+
+/**
+ * Created by Siim Kallari on 13/9/2015.
+ */
+public class KonsoolVäljund {
+    public static void main(String[] args) {
+        String nimi = "Siim";
+        int vanus = 28;
+        double palk = 1920.10;
+
+        System.out.printf("Kasutajanimi: %s, kes on %d aastat vana ning saab %f palka%n", nimi, vanus, palk);
+        System.out.printf("Programm:");
+    }
+}
