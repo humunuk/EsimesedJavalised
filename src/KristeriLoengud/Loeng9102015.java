05fbc0568ade9fce7538e2b1c2cbd502f12c1555
humunuk <siimkallari@gmail.com>
2015-10-09 13:13:27 +0300

    Lecture stuff, not really important

---
+++ src/KristeriLoengud/Loeng9102015.java
@@ -0,0 +1,24 @@
+package KristeriLoengud;
+
+/**
+ * Created by humunuk on 10/9/15.
+ */
+public class Loeng9102015 {
+
+    public static void main(String[] args) {
+        String so1 = "tere" + "hoi";
+        String so2 = "tere" + "hoi";
+
+        if (so1 == so2) {
+
+            System.out.println("Testime");
+
+        }
+
+        if (so1.equals(so2)) {
+            System.out.println("Testime2");
+        }
+    }
+
+
+}
